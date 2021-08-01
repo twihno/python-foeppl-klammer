760fad236c52e39a31654e984094b3f2c4e027d9
Thomas Schuster <twihno@gmail.com>
2021-08-01 22:08:21 +0200

    Added additional tests

--- tests/test_foepplklammer.py
+++ tests/test_foepplklammer.py
@@ -26,12 +26,22 @@ class TestFoeppl(unittest.TestCase):
                 np.array([0, 1, 9, 16, 9801]))
         )
 
+        self.assertEqual(
+            str(fp.foeppl_np(numbers, 0, 0)), str(
+                np.array([0, 1, 1, 1, 1]))
+        )
+
         with self.assertRaises(ValueError):
             fp.foeppl_np(error_numbers, 0, 2)
 
         self.assertEqual(
             str(fp.foeppl_np_inkl_null(error_numbers, 0, 2)),
             str(np.array([0, 0, 1, 4]))
+        )
+
+        self.assertEqual(
+            str(fp.foeppl_np_inkl_null(error_numbers, 0, 0)),
+            str(np.array([0, 0, 1, 1]))
         )
 
     def test_foepplklammer_standard(self):
@@ -42,6 +52,10 @@ class TestFoeppl(unittest.TestCase):
         self.assertEqual(foeppl(2, 0, 1), 2)
         self.assertEqual(foeppl(2, 0, 2), 4)
 
+        self.assertEqual(foeppl(-1, 0, 0), 0)
+        self.assertEqual(foeppl(1, 0, 0), 1)
+        self.assertEqual(foeppl(2, 0, 0), 1)
+
         with self.assertRaises(ValueError):
             foeppl(0, 0, 1)
 
@@ -50,6 +64,11 @@ class TestFoeppl(unittest.TestCase):
         self.assertEqual(foeppl_inkl_null(1, 0, 1), 1)
         self.assertEqual(foeppl_inkl_null(2, 0, 1), 2)
         self.assertEqual(foeppl_inkl_null(2, 0, 2), 4)
+
+        self.assertEqual(foeppl_inkl_null(-1, 0, 0), 0)
+        self.assertEqual(foeppl_inkl_null(0, 0, 0), 0)
+        self.assertEqual(foeppl_inkl_null(1, 0, 0), 1)
+        self.assertEqual(foeppl_inkl_null(2, 0, 0), 1)
 
 
 if __name__ == "__main__":
